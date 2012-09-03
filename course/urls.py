8c528d64be2d85f1b4e21912b3b458b3dd472fa2
Michael Sandford <sandford@compy586.(none)>
2012-09-03 11:23:58 -0500

    More debugging

--- course/urls.py
+++ course/urls.py
@@ -4,5 +4,5 @@ urlpatterns = patterns(
   'course.views',
   (r'^$', 'index'),
   url(r'^instructor/([^/]+)/$', 'instructor_detail',name='instructor_detail'),
-  url(r'^debug/(?P<id>\d+/$', 'debug_parsing',name='debug_parsing'),
+  url(r'^debug/(?P<id>\d+)/$', 'debug_parsing',name='debug_parsing'),
 )
