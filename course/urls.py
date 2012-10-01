c4b6221ea0f27540134900ed768ea7cfbabe0185
Michael Sandford <sandford@ufl.edu>
2012-10-01 12:27:18 -0500

    Wiring it up

--- course/urls.py
+++ course/urls.py
@@ -5,5 +5,6 @@ urlpatterns = patterns(
   (r'^$', 'index'),
   url(r'^instructor/([^/]+)/$', 'instructor_detail',name='instructor_detail'),
   url(r'^my-sessions/$', 'my_sessions',name='my_sessions'),
+  url(r'^all-sessions/$', 'all_sessions',name='all_sessions'),
   url(r'^debug/(?P<id>\d+)/$', 'debug_parsing',name='debug_parsing'),
 )
