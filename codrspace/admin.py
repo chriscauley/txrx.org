4ae24288895b474780a6bb690d64f994f731769e
Chris Cauley <chris@lablackey.com>
2012-12-18 15:47:34 -0500

    PostAdmin

--- codrspace/admin.py
+++ codrspace/admin.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 
 from .models import Post, Media
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+  list_display = ('__unicode__','author','featured','publish_dt')
+  list_editable = ('featured',)
+
+admin.site.register(Post,PostAdmin)
 admin.site.register(Media)
