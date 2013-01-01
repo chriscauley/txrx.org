97292604feac957a10417f9cead0dd36028d5412
Chris Cauley <chris@lablackey.com>
2013-01-01 18:38:41 -0500

    improving admin to show thumbnails

--- codrspace/admin.py
+++ codrspace/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from crop_override.admin import CropAdmin
+from sorl.thumbnail import get_thumbnail
 
 from .models import Post, Media
 
@@ -8,7 +9,12 @@ class PostAdmin(admin.ModelAdmin):
   list_editable = ('featured','status')
 
 class MediaAdmin(CropAdmin):
-  pass
+  list_display = ('__unicode__','_thumbnail')
+  def _thumbnail(self,obj):
+    im = get_thumbnail(obj.file,'100x100',crop='center')
+    out = '<img src="%s" width="%s" height="%s" />'%(im.url,im.width,im.height)
+    return out
+  _thumbnail.allow_tags=True
 
 admin.site.register(Post,PostAdmin)
 admin.site.register(Media,MediaAdmin)
