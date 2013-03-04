0772267b6e58a4f3ca7f4fae9d207b119e1e348a
Chris Cauley <chris@lablackey.com>
2013-03-04 01:44:21 -0500

    adding minutes page

---
+++ membership/migrations/0018_auto__add_unique_meetingminutes_date.py
@@ -0,0 +1,175 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding unique constraint on 'MeetingMinutes', fields ['date']
+        db.create_unique('membership_meetingminutes', ['date'])
+
+
+    def backwards(self, orm):
+        # Removing unique constraint on 'MeetingMinutes', fields ['date']
+        db.delete_unique('membership_meetingminutes', ['date'])
+
+
+    models = {
+        'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        'auth.permission': {
+            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'auth.user': {
+            'Meta': {'ordering': "['username']", 'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        'codrspace.photo': {
+            'Meta': {'object_name': 'Photo'},
+            'caption': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'file': ('crop_override.field.OriginalImage', [], {'max_length': '100', 'null': 'True'}),
+            'filename': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'instagramphoto': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['instagram.InstagramPhoto']", 'null': 'True', 'blank': 'True'}),
+            'landscape_crop': ('crop_override.field.CropOverride', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '500', 'null': 'True', 'blank': 'True'}),
+            'portrait_crop': ('crop_override.field.CropOverride', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'square_crop': ('crop_override.field.CropOverride', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'upload_dt': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']", 'null': 'True', 'blank': 'True'})
+        },
+        'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'instagram.instagramlocation': {
+            'Meta': {'object_name': 'InstagramLocation'},
+            'approved': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'follow': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'iid': ('django.db.models.fields.CharField', [], {'max_length': '32'}),
+            'latitude': ('django.db.models.fields.FloatField', [], {}),
+            'longitude': ('django.db.models.fields.FloatField', [], {}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '128'})
+        },
+        'instagram.instagramphoto': {
+            'Meta': {'ordering': "('-created_time',)", 'object_name': 'InstagramPhoto'},
+            'approved': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'caption': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'created_time': ('django.db.models.fields.IntegerField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'iid': ('django.db.models.fields.CharField', [], {'max_length': '32'}),
+            'instagram_location': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['instagram.InstagramLocation']", 'null': 'True', 'blank': 'True'}),
+            'instagram_tags': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'to': "orm['instagram.InstagramTag']", 'null': 'True', 'blank': 'True'}),
+            'instagram_user': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['instagram.InstagramUser']", 'null': 'True', 'blank': 'True'}),
+            'low_resolution': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'rejected': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'standard_resolution': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'thumbnail': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'})
+        },
+        'instagram.instagramtag': {
+            'Meta': {'object_name': 'InstagramTag'},
+            'approved': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'follow': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '128'})
+        },
+        'instagram.instagramuser': {
+            'Meta': {'object_name': 'InstagramUser'},
+            'approved': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'bio': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'follow': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'full_name': ('django.db.models.fields.CharField', [], {'max_length': '128', 'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'iid': ('django.db.models.fields.CharField', [], {'max_length': '32'}),
+            'profile_picture': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']", 'null': 'True', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'max_length': '128', 'null': 'True', 'blank': 'True'}),
+            'website': ('django.db.models.fields.URLField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'})
+        },
+        'membership.feature': {
+            'Meta': {'ordering': "('order',)", 'object_name': 'Feature'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'membership': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['membership.Membership']"}),
+            'order': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
+            'text': ('django.db.models.fields.CharField', [], {'max_length': '128'})
+        },
+        'membership.meetingminutes': {
+            'Meta': {'ordering': "('-date',)", 'object_name': 'MeetingMinutes'},
+            'content': ('wmd.models.MarkDownField', [], {}),
+            'date': ('django.db.models.fields.DateField', [], {'default': 'datetime.date.today', 'unique': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'member_count': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
+            'members_present': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.User']", 'symmetrical': 'False'})
+        },
+        'membership.membership': {
+            'Meta': {'ordering': "('order',)", 'object_name': 'Membership'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '64'}),
+            'order': ('django.db.models.fields.IntegerField', [], {})
+        },
+        'membership.membershiprate': {
+            'Meta': {'ordering': "('order',)", 'object_name': 'MembershipRate'},
+            'cost': ('django.db.models.fields.IntegerField', [], {}),
+            'description': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'membership': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['membership.Membership']"}),
+            'months': ('django.db.models.fields.IntegerField', [], {'default': '1'}),
+            'order': ('django.db.models.fields.IntegerField', [], {'default': '0'})
+        },
+        'membership.role': {
+            'Meta': {'object_name': 'Role'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '64'})
+        },
+        'membership.survey': {
+            'Meta': {'object_name': 'Survey'},
+            'expertise': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'projects': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'questions': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'reasons': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'skills': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']", 'unique': 'True'})
+        },
+        'membership.usermembership': {
+            'Meta': {'object_name': 'UserMembership'},
+            'bio': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'by_line': ('django.db.models.fields.CharField', [], {'max_length': '50', 'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'membership': ('django.db.models.fields.related.ForeignKey', [], {'default': '1', 'to': "orm['membership.Membership']"}),
+            'paypal_email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'null': 'True', 'blank': 'True'}),
+            'photo': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['codrspace.Photo']", 'null': 'True', 'blank': 'True'}),
+            'suspended': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'user': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['auth.User']", 'unique': 'True'}),
+            'voting_rights': ('django.db.models.fields.BooleanField', [], {'default': 'False'})
+        }
+    }
+
+    complete_apps = ['membership']
