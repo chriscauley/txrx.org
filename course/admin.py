c1cbfe0955f31ced5ac60a33c6e19fbd39c56b03
chriscauley <chris@lablackey.com>
2013-06-25 19:33:31 +0000

    broken search on enrollments

--- course/admin.py
+++ course/admin.py
@@ -47,7 +47,7 @@ class SessionAdmin(admin.ModelAdmin):
 class EnrollmentAdmin(admin.ModelAdmin):
   list_display = ("id",'user', 'session', )
   list_filter = ("session", "user",)
-  search_fields = ("user__username","user__email","user__profile__paypal_email")
+  search_fields = ("user__username","user__email","user__usermembership__paypal_email")
 
 admin.site.register(Subject,SubjectAdmin)
 admin.site.register(Course,CourseAdmin)
