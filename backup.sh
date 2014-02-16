d9b09b6126b6751666040bb307e57fa4ccfd6c75
chriscauley <chris@lablackey.com>
2014-02-16 09:56:53 -0500

    adding backup script

---
+++ backup.sh
@@ -0,0 +1,7 @@
+./manage.py dumpdata codrspace.pressitem thing>bu.json
+scp txrxlabs.org:/home/chriscauley/dump.sql ~/
+dropdb txrx --username=postgres;createdb txrx --username=postgres;psql --username=postgres txrx < ~/dump.sql
+./manage.py migrate
+./manage.py loaddata bu.json
+rm -rf .media
+scp -r txrxlabs.org:/home/website/txrx.org/src/media/* .media/*
