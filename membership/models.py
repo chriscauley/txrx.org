16e75f30c643da6ce192f707dbe78d7a7e1a7406
chriscauley <chris@lablackey.com>
2014-09-07 16:43:11 -0400

    arst

--- membership/models.py
+++ membership/models.py
@@ -199,3 +199,5 @@ class Survey(models.Model):
   skills = models.TextField(blank=True)
   expertise = models.TextField(blank=True)
   questions = models.TextField(blank=True)
+
+from listeners import *
