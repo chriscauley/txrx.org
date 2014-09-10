63e58fc87f3951e02b6bd33cc6961312e47a4ac3
chriscauley <chris@lablackey.com>
2014-09-10 16:33:01 -0400

    fault in django.contrib.auth.forms.UserCreationForm

--- user/forms.py
+++ user/forms.py
@@ -2,7 +2,26 @@ from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
 from .models import User
 
-class UserCreationForm(UserCreationForm):
+class CustomUserCreationForm(UserCreationForm):
+  #! These two fields methods are identical to the original but necessary because
+  #! original UserCreationForm uses django.contrib.auth.models.User instead of
+  #! get_user_model. Probably fixed in 1.7
+  def clean_username(self):
+    # Since User.username is unique, this check is redundant,
+    # but it sets a nicer error message than the ORM. See #13147.
+    username = self.cleaned_data["username"]
+    try:
+      User._default_manager.get(username=username)
+    except User.DoesNotExist:
+      return username
+    raise forms.ValidationError(self.error_messages['duplicate_username'])
+
+  def save(self, commit=True):
+    user = super(UserCreationForm, self).save(commit=False)
+    user.set_password(self.cleaned_data["password1"])
+    if commit:
+      user.save()
+    return user
   class Meta:
     model = User
     fields = ("username","email",)
