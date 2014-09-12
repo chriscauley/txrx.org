0764633cc43b273ff7b2fd64de14efeb4cf5024f
chriscauley <chris@lablackey.com>
2014-09-12 14:12:34 -0400

    moving conflicts to the admin

--- event/utils.py
+++ event/utils.py
@@ -2,7 +2,13 @@ from django.conf import settings
 from django.http import HttpResponse
 from django.template.defaultfilters import slugify
 
-import icalendar
+from course.models import ClassTime
+from geo.models import Location
+from .models import EventOccurrence
+
+import icalendar, datetime, math
+from itertools import groupby
+from operator import itemgetter
 from pytz import timezone,utc
 
 def make_ics(occurrences=None,title=None):
@@ -60,3 +66,75 @@ def ics2response(calendar_object,fname):
   response['Content-Length'] = len(icalstream)
 
   return response
+
+def iter_times(start,end):
+  kwargs = dict(second=0,microsecond=0)
+  # round back to the nearest half hour
+  start = start.replace(minute=start.minute-start.minute%30,**kwargs)
+  td = end - start
+  block_size = 60*30 #seconds per half hour
+  blocks = int(math.ceil(td.total_seconds()/(block_size))) #half hours that this runs
+  return [start+datetime.timedelta(0,block_size*i) for i in range(blocks)]
+
+def get_room_conflicts(td=30*60,base_occurrence=None):
+  if base_occurrence:
+    start_time = base_occurrence.start-datetime.timedelta(0,td)
+    end_time = base_occurrence.end+datetime.timedelta(0,td)
+    location = base_occurrence.location
+    class_times = ClassTime.objects.filter(start__gte=start_time,start__lte=end_time,
+                                           session__section__no_conflict=False,
+                                           section__location=location)
+    occurrences = EventOccurrence.objects.filter(start__gte=start_time,event__no_conflict=False,
+                                                 location=location)
+    locations = [base_occurrence.location]
+  else:
+    start_time = datetime.datetime.now()-datetime.timedelta(0,td)
+    end_time = datetime.datetime.now()+datetime.timedelta(60)
+    class_times = ClassTime.objects.filter(start__gte=start_time,start__lte=end_time,
+                                           session__section__no_conflict=False)
+    occurrences = EventOccurrence.objects.filter(start__gte=start_time,event__no_conflict=False)
+    locations = Location.objects.all()
+  schedule = {location:{} for location in locations}
+  event_tuples = []
+
+  # combine events and classes because they have similar enough APIs to treat them the same 
+  for class_time in class_times:
+    event_tuples.append((class_time,class_time.session.section.location))
+  for occurrence in occurrences:
+    event_tuples.append((occurrence, occurrence.get_location()))
+
+  # iterate over 30 minute chunks and group chunks by time and location
+  for event,location in event_tuples:
+    for time in iter_times(event.start,event.end):
+      schedule[location][time] = schedule[location].get(time,[])
+      schedule[location][time].append(event)
+
+  # remove all slots with only one for fewer events in a given room
+  room_conflicts = {}
+  for location,event_slots in schedule.items():
+    room_conflicts[location] = []
+    for dt,events in event_slots.items():
+      if not events or len(events) == 1:
+        continue
+      room_conflicts[location].append((dt,events))
+
+  # re-group the 30 minute chunks by consecutive slots in a given room
+  room_conflicts = [r for r in room_conflicts.items() if r[1]]
+  out = []
+  for location,conflicts in room_conflicts:
+    reshuffled_conflicts = []
+    location_conflicts = []
+    conflicts.sort(key=lambda i:i[0])
+    func = lambda (i,(dt,events)): dt-datetime.timedelta(0,i*60*30)
+    for k,g in groupby(enumerate(conflicts),key=func):
+      reshuffled_conflicts.append(map(itemgetter(1), g))
+    for conflict_spots in reshuffled_conflicts:
+      times,_event_mess = zip(*conflict_spots)
+      times = [times[0], times[-1]]
+      events = []
+      for e in _event_mess:
+        events += e
+      events = list(set(events))
+      location_conflicts.append((times,events))
+    out.append((location,location_conflicts))
+  return out
