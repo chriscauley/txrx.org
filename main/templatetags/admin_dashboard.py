897fb863b0d99a19c8014c1ced7aca9a7b8c79b8
chriscauley <chris@lablackey.com>
2014-09-13 22:04:36 -0400

    bug in dashboard

--- main/templatetags/admin_dashboard.py
+++ main/templatetags/admin_dashboard.py
@@ -8,7 +8,7 @@ register = Library()
 @register.simple_tag(takes_context=True)
 def get_room_conflicts(context):
   user = context['request'].user
-  if not (user.is_superuser or user.group_set.filter(name="Event Coordinator")):
+  if not (user.is_superuser or user.groups.filter(name="Event Coordinator")):
     return ''
   context['room_conflicts'] = _get_room_conflicts()
   return ''
