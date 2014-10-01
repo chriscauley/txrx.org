2fed688a6919075c876855413f061c6135d353a4
chriscauley <chris@lablackey.com>
2014-09-30 23:09:51 -0400

    hiding smaller conflicts (<30 minutes)

--- event/utils.py
+++ event/utils.py
@@ -132,6 +132,8 @@ def get_room_conflicts(base_occurrence=None):
     for conflict_spots in reshuffled_conflicts:
       times,_event_mess = zip(*conflict_spots)
       times = [times[0], times[-1]]
+      if (times[-1] - times[0]).total_seconds() < 30*60:
+        continue
       events = []
       for e in _event_mess:
         events += e
