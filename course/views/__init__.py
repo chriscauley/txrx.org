b8629a7877141be851b7a450e92248bc8c6f9aba
chriscauley <chris@lablackey.com>
2014-11-05 12:46:52 -0500

    sessions can now be deactivated so they dont show up on the site

--- course/views/__init__.py
+++ course/views/__init__.py
@@ -1,62 +1,11 @@
-from django.core.urlresolvers import reverse
-from django.contrib.auth.decorators import login_required
 from django.contrib.auth import get_user_model
-from django.contrib.admin.views.decorators import staff_member_required
-from django.contrib import messages
 from django.http import QueryDict, Http404, HttpResponseRedirect
-from django.shortcuts import get_object_or_404
 from django.template.response import TemplateResponse
 
-from ..models import Course, Section, Term, Subject, Session, Enrollment, ClassTime
-from ..forms import EmailInstructorForm, EvaluationForm
+from ..models import Session
 from db.utils import get_or_none
-from event.utils import make_ics,ics2response
-from membership.models import UserMembership
 
 from paypal.standard.ipn.models import *
-
-@login_required
-def my_sessions(request):
-  instructor = request.user
-  current_term = Term.objects.all()[0]
-  terms = Term.objects.all()
-  enrollment_tuples = []
-  teaching_tuples = []
-  for term in terms:
-    enrollments = Enrollment.objects.filter(user=request.user,session__section__term=term)
-    sessions = Session.objects.filter(user=instructor,section__term=term)
-    if enrollments:
-      enrollment_tuples.append((enrollments,term))
-    if sessions:
-      teaching_tuples.append((sessions,term))
-  values = {
-    'instructor': instructor,
-    'teaching_tuples': teaching_tuples,
-    'enrollment_tuples': enrollment_tuples,
-    'current_term': current_term,
-    }
-  return TemplateResponse(request,"course/my_sessions.html",values)
-  
-@staff_member_required
-def all_sessions(request):
-  current_term = Term.objects.all()[0]
-  sessions = Session.objects.filter(section__term=current_term)
-  
-  total_fees = 0
-  total_enrollments = 0
-  for session in sessions:
-      enrollments = session.enrollment_set.count()
-      class_price = session.section.fee
-      total_enrollments += enrollments
-      total_fees += enrollments * class_price
-  
-  values = {
-    'sessions': sessions,
-    'current_term': current_term,
-    'total_enrollments': total_enrollments,
-    'total_fees': total_fees,
-    }
-  return TemplateResponse(request,"course/all_sessions.html",values)
 
 def debug_parsing(request, id):
   ipn = PayPalIPN.objects.get(id=id)
