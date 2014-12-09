9ea183a4ba4cf8863bc6a2262b5d98c221b042f1
chriscauley <chris@lablackey.com>
2014-12-09 01:36:35 -0500

    admin schmadmin

--- notify/admin.py
+++ notify/admin.py
@@ -4,7 +4,7 @@ from .models import NotifyCourse
 
 class NotifyCourseAdmin(admin.ModelAdmin):
   list_display = ("__unicode__","_enrolled")
-  raw_id_fields = ("user","course","session")
+  raw_id_fields = ("user","course")
   def _enrolled(self,obj):
     return obj.user.enrollment_set.filter(session__section__course=obj.course).count()
 
