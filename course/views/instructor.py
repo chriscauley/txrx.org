5ac22544e691639b48cb7d2ad4a432727f9536d0
chriscauley <chris@lablackey.com>
2015-01-14 15:27:35 -0500

    redirect missing

--- course/views/instructor.py
+++ course/views/instructor.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404
 from django.template.response import TemplateResponse
 
