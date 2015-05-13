789e0c30e9f8e8e029f8c8b14341869d1b2cd43f
chriscauley <chris@lablackey.com>
2015-05-13 14:58:27 -0400

    removing a few depracated settings

--- txrx/settings/apps.py
+++ txrx/settings/apps.py
@@ -70,28 +70,6 @@ SITE_TAGLINE = "Houston's Hackerspace"
 VERSION = "0.1 Alpha"
 ANALYTICS_CODE = ''
 
-from frappy.services.github import Github
-
-# Information for correct authentication with github
-# Replace with information specific to your app, etc.
-GITHUB_AUTH = {
-  'client_id': '',
-  'secret': '',
-  'callback_url': 'http://dev.txrxlabs.org:8009/blog/signin_callback/',
-  'auth_url': 'https://github.com/login/oauth/authorize',
-  'access_token_url': 'https://github.com/login/oauth/access_token',
-  
-  # Get information of authenticated user
-  'user_url': 'https://api.github.com/user',
-  
-  # Debug mode - for faking auth_url
-  'debug': False
-  }
-
-# set this to the JSON that comes back from http://api.github.com/user/
-#user = Github().users(GITHUB_AUTH['username'])
-#GITHUB_USER_JSON = user.response_json
-
 INSTAGRAM_TAG = "txrx"
 INSTAGRAM_EMAIL = ['chris@lablackey.com']
 INSTAGRAM_TOKEN = "3794301.f59def8.e08bcd8b10614074882b2d1b787e2b6f"
