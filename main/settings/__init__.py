fc9c69cab8d6c2d5faa2d821071aa9e721991d21
chriscauley <chris@lablackey.com>
2015-06-09 02:22:53 -0400

    more spring cleaning

--- main/settings/__init__.py
+++ main/settings/__init__.py
@@ -48,7 +48,6 @@ MIDDLEWARE_CLASSES = (
   'django.middleware.csrf.CsrfViewMiddleware',
   'django.contrib.auth.middleware.AuthenticationMiddleware',
   'django.contrib.messages.middleware.MessageMiddleware',
-  'theme.middleware.Theme',
 )
 
 AUTH_USER_MODEL = 'user.User'
