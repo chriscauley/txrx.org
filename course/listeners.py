20442813e3273ee1ad82d9c6f5f61fe1cf813a91
chriscauley <chris@lablackey.com>
2015-08-06 15:08:36 -0400

    removing one of the notfications from the course listener

--- course/listeners.py
+++ course/listeners.py
@@ -95,8 +95,6 @@ def handle_successful_payment(sender, **kwargs):
     if new:
       enrollment.quantity = quantity
     else:
-      s = "%s seems to have a duplicate enrollment. Please see https://txrxlabs.org/admin/course/session/%s/"
-      mail_admins("Course so nice they took it twice",s%(enrollment.user,enrollment.session.pk))
       enrollment.quantity += quantity
     enrollment.save()
     enrollments.append(enrollment)
