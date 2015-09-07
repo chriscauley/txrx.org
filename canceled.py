6788039fab21a026c118038fd514020cb3fd7ad7
chriscauley <chris@lablackey.com>
2015-09-07 14:57:12 -0400

    more cancellation automation

--- canceled.py
+++ canceled.py
@@ -8,19 +8,34 @@ from membership.models import Subscription, Membership, add_months
 Subscription.objects.all().update(canceled=None)
 
 now = datetime.datetime.now()
+low = high = other = 0
+initial = Subscription.objects.filter(owed__gt=0).count()
+
+for subscription in Subscription.objects.all():
+  subscription.recalculate()
 
 for membership in Membership.objects.filter(order__gte=1):
   for subscription in Subscription.objects.filter(product__membership=membership):
+    if not subscription.owed > 0:
+      continue
     if membership.order in [1,2]: #amigotron/supporter
-      subscription.recalculate()
-      if subscription.owed > 0:
-        subscription.force_canceled()
-    else:
-      last = subscription.last_status
-      if (now - last.datetime).days > 365:
-        subscription.force_canceled()
+      low += 1
+      subscription.force_canceled()
+    elif (now - subscription.last_status.datetime).days > 365:
+      high += 1
+      subscription.force_canceled()
+    elif subscription.user.subscription_set.filter(owed__lte=0).exclude(pk=subscription.pk):
+      subscription.force_canceled()
+      other += 1
+  print membership,'\t',Subscription.objects.filter(product__membership=membership,owed__gt=0).count()
 
-cancel_all=True
+print 'init:\t',initial
+print 'low:\t',low
+print 'high:\t',high
+print 'other:\t',other
+print 'remain:\t',Subscription.objects.filter(owed__gt=0).count()
+
+cancel_all = False
 
 if cancel_all:
   for s in Subscription.objects.filter(owed__gt=0):
