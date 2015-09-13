ebad580bd8c359e2a5d503dd46163b51e93853be
chriscauley <chris@lablackey.com>
2015-09-12 20:49:51 -0400

    removing unused chore app

--- chore/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import patterns, include, url
-import os
-urlpatterns = patterns(
-  'chore.views',
-  url(r'^$','index',name='index'),
-  url(r'^assign/(\d+)/$', "assign", name="assign"),
-  url(r'^complete/(\d+)/$', "complete", name="complete"),
-)
