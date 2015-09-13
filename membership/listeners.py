a982652ccdfb3279acc278428e3133e2622eb60b
chriscauley <chris@lablackey.com>
2015-09-13 10:54:15 -0400

    sending half functioning subscripiton signup so that I can probe backend

--- membership/listeners.py
+++ membership/listeners.py
@@ -52,12 +52,11 @@ def handle_successful_membership_payment(sender,**kwargs):
 _duid2='membership.listners.handle_subscription_signup'
 @receiver(subscription_signup, dispatch_uid=_duid2)
 def handle_subscription_signup(sender,**kwargs):
-  if sender.txn_type != "subscr_payment":
-    print "\n\n\nNOT A SUBSCRIPTION PAYMENT\n\n\n"
-    return # not a membership payment
-  if MembershipChange.objects.filter(transaction_id=sender.txn_id):
-    return # This has already been processed
   params = QueryDict(sender.query)
+  if sender.txn_type != "subscr_payment":
+    return # not a membership payment
+  if Subscription.objects.filter(subscr_id=params.get('subscr_id')):
+    return # This has already been processed
   user,new_user = get_or_create_student(sender.payer_email,subscr_id=params.get('subscr_id',None))
   try:
     membership = Membership.objects.get(name=params.get('option_name1',''))
@@ -65,3 +64,5 @@ def handle_subscription_signup(sender,**kwargs):
     b = "Could not find membership %s for txn %s"%(params.get('option_name1',''),sender.txn_id)
     mail_admins("Bad IPN",b)
     return
+  """Subscription(
+    membership=membership"""
