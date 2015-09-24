3bf98397915c3805f8a8207d4d85f79521f4db7c
chriscauley <chris@lablackey.com>
2015-09-24 16:47:52 -0400

    removing old flag template

--- main/templatetags/admin_dashboard.py
+++ main/templatetags/admin_dashboard.py
@@ -2,7 +2,6 @@ from django.template import Library
 
 from course.models import Course
 from event.utils import get_room_conflicts as _get_room_conflicts
-from membership.models import Flag
 
 import datetime
 
@@ -24,22 +23,4 @@ def get_room_conflicts(context):
 @register.simple_tag(takes_context=True)
 def get_courses_needed(context):
   context['courses_needed'] = Course.objects.courses_needed()
-  return ''
-  
-@register.simple_tag(takes_context=True)
-def get_user_flags(context):
-  today, upcoming, pastdue = [], [], []
-  now = datetime.datetime.now()
-  for flag in Flag.objects.filter(status__in=Flag.ACTION_CHOICES):
-    if flag.date_of_next_action > now:
-      upcoming.append(flag)
-    elif flag.date_of_next_action < now:
-      pastdue.append(flag)
-    else:
-      today.append(flag)
-  context['user_flags'] = [
-    ['Action Past Due',pastdue,'danger'],
-    ['Todays Flags',today,'success'],
-    ['Upcoming Flags',upcoming,'']
-  ]
   return ''
