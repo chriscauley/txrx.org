8e1a892c9fa57ff011f1d065b0dae2c3130eedc1
chriscauley <chris@lablackey.com>
2015-10-27 16:28:01 -0400

    redtape.signature is now a criterion model

--- redtape/models.py
+++ redtape/models.py
@@ -1,6 +1,8 @@
 from django.conf import settings
 from django.db import models
 
+from tool.models import CriterionModel
+
 class Document(models.Model):
   name = models.CharField(max_length=64)
   content = models.TextField(null=True,blank=True)
@@ -8,11 +10,19 @@ class Document(models.Model):
   login_required = models.BooleanField(default=False,help_text=_ht)
   __unicode__ = lambda self: self.name
 
-class Signature(models.Model):
+class Signature(CriterionModel):
   document = models.ForeignKey(Document)
+  automatic = True
   date_typed = models.CharField("Type Todays Date",max_length=64)
   name_typed = models.CharField("Type Your Name",max_length=128)
   signature = models.ImageField(upload_to="signatures/%m-%d-%y")
   user = models.ForeignKey(settings.AUTH_USER_MODEL,null=True,blank=True)
-  date = models.DateField(auto_now_add=True)
+  get_criteria = lambda self: self.document.criterion_set.all()
   __unicode__ = lambda self: "%s - %s"%(self.name_typed,self.date_typed)
+  @property
+  def as_json(self):
+    return {
+      'id': self.id,
+      'document_name': unicode(self.document),
+      'completed': unicode(self.completed or ''),
+    }
--- redtape/views.py
+++ redtape/views.py
@@ -1,4 +1,5 @@
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404
 from django.template.response import TemplateResponse
@@ -8,9 +9,14 @@ from .forms import SignatureForm
 
 def document_detail(request,document_pk):
   document = get_object_or_404(Document,pk=document_pk)
+  if document.login_required and not request.user.is_authenticated():
+    return login_required(document_detail)(request,document_pk)
   form = SignatureForm(request.POST or None,request.FILES or None,initial={'document':document})
   if form.is_valid():
-    signature = form.save()
+    signature = form.save(commit=False)
+    if request.user.is_authenticated():
+      signature.user = request.user
+    signature.save()
     messages.success(request,"%s signed by %s"%(document,signature.name_typed))
     return HttpResponseRedirect('')
   values = {
