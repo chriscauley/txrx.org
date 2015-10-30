e6eedaae4c2a70f4ad89fe98ef0f7704cc956c45
chriscauley <chris@lablackey.com>
2015-10-30 15:19:50 -0400

    enrollment form was broken

--- course/views/instructor.py
+++ course/views/instructor.py
@@ -8,6 +8,8 @@ from ..models import Session
 from ..forms import EmailInstructorForm
 from lablackey.utils import FORBIDDEN
 
+import datetime
+
 @login_required
 def email(request,session_pk):
   session = get_object_or_404(Session,pk=session_pk)
@@ -32,7 +34,10 @@ def session(request,session_pk):
       return HttpResponseRedirect(request.path)
     ids = [int(i) for i in request.POST.getlist('completed')]
     for enrollment in session.enrollment_set.all():
-      enrollment.completed = enrollment.id in ids
+      if enrollment.id in ids:
+        enrollment.completed = datetime.datetime.now()
+      elif enrollment.completed:
+        enrollment.completed = None
       enrollment.save()
     messages.success(request,"Course completion status saved for all students in this class.")
     return HttpResponseRedirect(request.path)
