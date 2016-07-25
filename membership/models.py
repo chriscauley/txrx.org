f29928b842b12e0ed5491931676de5e30bd680b9
chriscauley <chris@lablackey.com>
2016-07-25 12:04:52 -0400

    changing default for container to used

--- membership/models.py
+++ membership/models.py
@@ -44,7 +44,7 @@ class Container(models.Model):
   number = models.IntegerField()
   room = models.ForeignKey('geo.Room')
   user = models.ForeignKey(settings.AUTH_USER_MODEL,null=True,blank=True)
-  status = models.CharField(max_length=16,choices=CONTAINER_STATUS_CHOICES,default="open")
+  status = models.CharField(max_length=16,choices=CONTAINER_STATUS_CHOICES,default="used")
   kind = models.CharField(max_length=64,choices=KIND_CHOICES,default='bay')
   __unicode__ = lambda self: "%s %s #%s - %s"%(self.room,self.get_kind_display(),self.number,self.user)
   class Meta:
