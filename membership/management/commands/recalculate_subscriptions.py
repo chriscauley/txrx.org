7e0386c996edbcab427c3a434a26a67ebd6b7256
chriscauley <chris@lablackey.com>
2016-09-26 16:12:30 -0400

    people canceled in the future were not being calculated

--- membership/management/commands/recalculate_subscriptions.py
+++ membership/management/commands/recalculate_subscriptions.py
@@ -1,8 +1,13 @@
+from django.conf import settings
 from django.core.management.base import BaseCommand
 
 from membership.models import Subscription
 
 class Command(BaseCommand):
   def handle(self, *args, **options):
-    [s.recalculate() for s in Subscription.objects.filter(canceled__isnull=True)]
-    print "Calculated %s subscriptions"%Subscription.objects.count()
+    #We don't care about canceled subscriptions that have been set to 0 owed and users reset to non-paying
+    ss = Subscription.objects.exclude(canceled__isnull=True,user__level=settings.DEFAULT_MEMBERSHIP_LEVEL,owed=0)
+    print "Calculating %s subscriptions"%ss.count()
+    for s in ss:
+      s.recalculate()
+    print "%s subscriptions remain"%ss.count()
--- membership/views.py
+++ membership/views.py
@@ -209,7 +209,8 @@ def door_access(request):
     return fail
   valid = valid or request.user.is_superuser
 
-  base_subs = Subscription.objects.filter(canceled__isnull=True,owed__lte=0)
+  _Q = Q(canceled__isnull=True) | Q(canceled__gte=datetime.datetime.now())
+  base_subs = Subscription.objects.filter(_Q,owed__lte=0)
   base_subs = base_subs.exclude(user__rfid__isnull=True)
 
   obj = None
