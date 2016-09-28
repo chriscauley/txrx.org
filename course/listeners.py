075545cdf9faf9f16d4b82e7894a7b7db809db2f
chriscauley <chris@lablackey.com>
2016-09-28 16:38:09 -0400

    cart ipn was bayd

--- course/listeners.py
+++ course/listeners.py
@@ -35,7 +35,7 @@ def handle_successful_store_payment(sender, user):
     total += int(float(params['mc_gross_%d'%i]))
     quantity = int(params['quantity%s'%i])
     try:
-      product = Product.objects.get(pk=int(params['item_number%d'%i]))
+      product = Product.objects.get(pk=params['item_number%d'%i])
     except Product.DoesNotExist:
       mail_admins("Product fail for %s"%sender.txn_id,"")
       continue
