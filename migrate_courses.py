ccaba4e6eb8e2ed2a0e66697bb317b1386ddefe6
chriscauley <chris@lablackey.com>
2016-10-15 14:49:02 -0400

    adding migrate courses and market analysis scripts

---
+++ migrate_courses.py
@@ -0,0 +1,62 @@
+import os,django;os.environ['DJANGO_SETTINGS_MODULE']='main.settings';django.setup()
+
+from course.models import Course, CourseEnrollment
+from store.models import Consumable, CourseCheckout
+from tool.models import UserCriterion
+
+import datetime
+
+consumable_course_ids = [
+  (948, 37),
+  (949, 6),
+  (857, 48),
+  (1029, 62),
+  (1105, 180),
+  (858, 63),
+  (856, 3),
+  (952, 95),
+  (855, 42),
+]
+
+unpaid = 0
+nouser = 0
+
+for consumable_id,course_id in consumable_course_ids:
+  consumable = Consumable.objects.get(id=consumable_id)
+  course = Course.objects.get(id=course_id)
+  coursecheckout,new = CourseCheckout.objects.get_or_create(
+    name=consumable.name.split(" Check")[0],
+    active=True,
+    unit_price=consumable.unit_price,
+    course=course
+  )
+  if new:
+    print "new CC: %s"%coursecheckout
+
+  orderitems = consumable.orderitem_set.all()
+  for oi in orderitems:
+    oi.product_name = coursecheckout.get_name()
+    oi.product_reference = coursecheckout.get_product_reference()
+    oi.product = coursecheckout
+    oi.save()
+    user = oi.order.user
+    if not user:
+      nouser += 1
+      continue
+    if not oi.order.is_paid():
+      unpaid += 1
+      continue
+    coursecheckout.purchase(oi.order.user,oi.quantity)
+    ce = CourseEnrollment.objects.get(user=oi.order.user,course=course)
+    ucs = UserCriterion.objects.filter(user=oi.order.user,criterion=ce.get_criteria())
+    if ucs:
+      for uc in ucs:
+        print ucs.count(),'\t',uc.content_object._meta.model_name,'\t',uc.content_object
+        if uc.content_object._meta.model_name == 'user':
+          ce.completed = datetime.datetime.now()
+          ce.save()
+          print "converting %s"%ce
+  print orderitems.count(),'\t',consumable
+
+print "unpaid: ",unpaid
+print "nouser: ",nouser
---
+++ scripts/market.py
@@ -0,0 +1,31 @@
+import os, django, datetime, arrow
+os.environ['DJANGO_SETTINGS_MODULE'] = 'main.settings'
+
+django.setup()
+from membership.models import Subscription, Level, Status
+from django.db.models import Q
+
+levels = list(Level.objects.all())[1:-3]
+rows = [["Year","Month"] + [str(p) for p in levels] + [""] + [str(p) for p in levels] + ["total"]]
+
+for year in range(2012,2017):
+  for month in range(1,13):
+    row = [str(year),str(month)]
+    cash = [""]
+    start = arrow.get(year,month,1).datetime
+    end = arrow.get(year,month,1).replace(months=1).datetime
+    if start > arrow.get():
+      break
+    for level in levels:
+      subscriptions = Subscription.objects.filter(created__lte=start)
+      subscriptions = subscriptions.filter(Q(canceled__gte=start) | Q(canceled=None),product__level=level).count()
+      row.append(str(subscriptions))
+      statuses = Status.objects.filter(datetime__gte=start,datetime__lte=end)
+      statuses = statuses.filter(subscription__product__level=level).distinct()
+      cash.append(str(sum([s.amount for s in statuses])))
+    statuses = Status.objects.filter(datetime__gte=start,datetime__lte=end)
+    cash.append(str(sum([s.amount for s in statuses])))
+    row += cash
+    rows.append(row)
+
+print "\n".join([','.join(r) for r in rows])
