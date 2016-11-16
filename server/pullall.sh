c022c53e80315d552b2fe221deb37726b0408696
chriscauley <chris@lablackey.com>
2016-11-16 14:48:15 -0500

    putting jquery, unrest, and django javascript under version control

--- server/pullall.sh
+++ server/pullall.sh
@@ -3,9 +3,9 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export PROJECT_ROOT=$DIR/../../
 
-for d in lablackey django-drop unrest django-unrest-media
+for d in `ls .dev`
 do
-    cd  $PROJECT_ROOT$d
+    #cd  $PROJECT_ROOT$d
     printf "updating $d ... ... "
     git pull
 done
