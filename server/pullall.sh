511ae77be1ee068488c4991611169c3345cfa13e
chriscauley <chris@lablackey.com>
2016-11-20 15:43:49 -0500

    updating pull all script

--- server/pullall.sh
+++ server/pullall.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-export PROJECT_ROOT=$DIR/../../
+export PROJECT_ROOT=$DIR/../
 
-for d in `ls .dev`
+for d in `ls $PROJECT_ROOT.dev`
 do
-    #cd  $PROJECT_ROOT$d
+    cd  $PROJECT_ROOT.dev/$d
     printf "updating $d ... ... "
     git pull
 done
