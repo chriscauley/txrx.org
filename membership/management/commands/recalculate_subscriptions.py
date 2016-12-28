1c83f0b813dde35e69f143b177dc583619a1781b
chriscauley <chris@lablackey.com>
2016-12-27 19:58:26 -0500

    had recalculate backwards on subscripiton

--- membership/management/commands/recalculate_subscriptions.py
+++ membership/management/commands/recalculate_subscriptions.py
@@ -6,7 +6,7 @@ from membership.models import Subscription
 class Command(BaseCommand):
   def handle(self, *args, **options):
     #We don't care about canceled subscriptions that have been set to 0 owed and users reset to non-paying
-    ss = Subscription.objects.exclude(canceled__isnull=True,user__level=settings.DEFAULT_MEMBERSHIP_LEVEL,owed=0)
+    ss = Subscription.objects.exclude(canceled__isnull=False,user__level=settings.DEFAULT_MEMBERSHIP_LEVEL,owed=0)
     print "Calculating %s subscriptions"%ss.count()
     for s in ss:
       s.recalculate()
