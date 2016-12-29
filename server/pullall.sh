2a651f4cb141d5983a1579b1f98e18eba8b78cf2
chriscauley <chris@lablackey.com>
2016-12-29 13:42:57 -0500

    place favico in ready function

--- server/pullall.sh
+++ server/pullall.sh
@@ -6,6 +6,7 @@ export PROJECT_ROOT=$DIR/../
 for d in django-drop django-airbrake-lite dj-stripe lablackey unrest django-unrest-comments
 do
     cd  ~/$d -P
+    unset GIT_DIR
     printf "updating $(pwd) ... ... "
     git pull
 done
