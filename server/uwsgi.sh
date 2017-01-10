1eb451342f6c62605e12007ceee8b374bdc0ecb9
chriscauley <chris@lablackey.com>
2017-01-10 02:08:50 +0000

    adding a method to override uwsgi process number via presence of a file

--- server/uwsgi.sh
+++ server/uwsgi.sh
@@ -1,10 +1,19 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+export PROCESSES=3
+if [ -a $DIR/staging ]
+then
+    export PROCESSES=1
+fi
+
+echo /usr/bin/uwsgi-core -M -x $DIR/../uwsgi.xml --plugin python -p $PROCESSES
+
 if cat /tmp/uwsgi.pid
 then
     kill -HUP `cat /tmp/uwsgi.pid`
     echo "uwsgi reset"
 else
-    /usr/bin/uwsgi-core -M -x $DIR/../uwsgi.xml --plugin python&&
+    /usr/bin/uwsgi-core -M -x $DIR/../uwsgi.xml --plugin python -p $PROCESSES &&
+    ps aux|grep uwsgi
     echo "uwsgi wasn't running, but it is now!"
 fi
 
