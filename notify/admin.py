8e9d4027c1ab3df57571a6a5f5a47e45bca465bf
chriscauley <chris@lablackey.com>
2017-02-21 17:18:12 -0500

    follow admin

--- notify/admin.py
+++ notify/admin.py
@@ -1,12 +1,8 @@
 from django.contrib import admin
 
+from .models import Follow, Notification
 from lablackey.db.admin import RawMixin
-from .models import NotifyCourse
 
-class NotifyCourseAdmin(RawMixin,admin.ModelAdmin):
-  list_display = ("__unicode__","_enrolled")
-  raw_id_fields = ("user","course")
-  def _enrolled(self,obj):
-    return obj.user.enrollment_set.filter(session__course=obj.course).count()
-
-admin.site.register(NotifyCourse,NotifyCourseAdmin)
+@admin.register(Follow)
+class FollowAdmin(RawMixin,admin.ModelAdmin):
+  list_display = ("__unicode__",)
