cb0857582bdbfbd0cc241a2ae416d327a1149d03
chriscauley <chris@lablackey.com>
2017-02-24 16:31:50 -0500

    adding some html/styling to notifcation button while also hiding the notification button

--- notify/context.py
+++ notify/context.py
@@ -7,9 +7,9 @@ def process(request):
   if not request.user.is_authenticated():
     return {}
   request.user.notification_set.filter(read__isnull=True,url=request.path).update(read=timezone.now())
-  return {
-    'unread_notifications': request.user.notification_set.filter(
-      read__isnull=True,
-      datetime__gte=timezone.now()-datetime.timedelta(getattr(settings,"NOTIFICATION_EXPIRY_DAYS",7))
-    ),
-  }
+  return {}
+  #  'unread_notifications': request.user.notification_set.filter(
+  #    read__isnull=True,
+  #    datetime__gte=timezone.now()-datetime.timedelta(getattr(settings,"NOTIFICATION_EXPIRY_DAYS",7))
+  #  ),
+  #}
