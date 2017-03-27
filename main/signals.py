41cb397f86423a733efe2a6281dc655185855561
chriscauley <chris@lablackey.com>
2017-03-27 18:56:02 -0400

    adding emails back into comment signal

--- main/signals.py
+++ main/signals.py
@@ -3,6 +3,7 @@ from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.core.mail import send_mail, mail_admins
 from django.core.urlresolvers import reverse
+from django.utils import timezone
 from django.db.models.signals import post_save
 
 from unrest_comments.models import UnrestComment
@@ -33,14 +34,29 @@ def new_comment_connection(sender, instance=None, created=False,**kwargs):
     url = instance.content_object.get_absolute_url()
   except AttributeError:
     url = None
+  _kwargs = {
+    'target_type': 'unrest_comments.unrestcomment',
+    'target_id': instance.id,
+    'url': url,
+    'emailed': timezone.now(),
+  }
   if instance.parent:
     # email whoever wrote the parent comment
     user = instance.parent.user
     Notification.objects.create(
       message='Comment Reply on "%s" from %s'%(instance.content_object,instance.user),
-      url=url,
       user=user,
       relationship='comment_reply',
+      **_kwargs
+    )
+    key = LimitedAccessKey.new(user).key
+    _dict['unsubscribe_url'] = _u(reverse("unsubscribe",args=['comments',user.id])+"?LA_KEY="+key)
+    subject = 'Someone responded to your comment'
+    send_mail(
+      subject,
+      comment_response_email%_dict,
+      settings.DEFAULT_FROM_EMAIL,
+      [user.email]
     )
   else:
     # email the course instructor or whoever is in the "users" list
@@ -52,9 +68,11 @@ def new_comment_connection(sender, instance=None, created=False,**kwargs):
     for user in users:
       Notification.objects.create(
         message='New comment on %s from %s'%(instance.content_object,instance.user),
-        url=url,
         user=user,
         relationship='new_comment',
+        **_kwargs
       )
+      subject = 'New comment on %s'%instance.content_object
+      send_mail(subject,admin_comment_email%_dict,settings.DEFAULT_FROM_EMAIL,list(set(users)))
 
 post_save.connect(new_comment_connection, sender=UnrestComment)
