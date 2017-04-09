26f9ad77fb1f758db9a524129c9ff7c23f7bebbe
chriscauley <chris@lablackey.com>
2017-04-08 22:12:04 -0400

    comment was sending multiple times

--- main/signals.py
+++ main/signals.py
@@ -73,6 +73,6 @@ def new_comment_connection(sender, instance=None, created=False,**kwargs):
         **_kwargs
       )
       subject = 'New comment on %s'%instance.content_object
-      send_mail(subject,admin_comment_email%_dict,settings.DEFAULT_FROM_EMAIL,list(set([u.email for u in users])))
+      send_mail(subject,admin_comment_email%_dict,settings.DEFAULT_FROM_EMAIL,[user.email])
 
 post_save.connect(new_comment_connection, sender=UnrestComment)
