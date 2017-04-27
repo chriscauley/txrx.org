f4155cdbbb5450c6dbf035b4cf5fe3a2a1069cb9
chriscauley <chris@lablackey.com>
2017-04-27 15:17:38 -0400

    testing out db cache

--- main/settings/__init__.py
+++ main/settings/__init__.py
@@ -156,8 +156,8 @@ PUBLIC_SETTINGS = ["DEBUG","SITE_NAME","SITE_DOMAIN","SITE_URL", "MEMBERSHIP_EMA
 
 CACHES = { # only using this for ics, should be enough for now
   'default': {
-    'BACKEND': 'django.core.cache.backends.filebased.FileBasedCache',
-    'LOCATION': '/var/tmp/django_cache',
+    'BACKEND': 'django.core.cache.backends.db.DatabaseCache',
+    'LOCATION': 'my_table_name',
   }
 }
 
