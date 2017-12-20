4fe5b78764262d5cfa7dbac86a5f5e067891f9a6
chriscauley <chris@lablackey.com>
2017-12-20 01:14:23 +0000

    dont automatically pull repositories

--- server/build.sh
+++ server/build.sh
@@ -4,19 +4,23 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export PROJECT_ROOT=$DIR/../
 
 cd $PROJECT_ROOT
-MIGRATIONS="$(bash server/pullall.sh|grep migrations/00)"
+python manage.py migrate >/dev/null
+gulp > /dev/null
 
-if [[ ! -z $MIGRATIONS ]];
-    then
-    echo "migrating because of git pull output:"
-    echo $MIGRATIONS
-    cd $PROJECT_ROOT
-    python manage.py migrate >/dev/null
-fi
+#removing pullall and gulps
+# MIGRATIONS="$(bash server/pullall.sh|grep migrations/00)"
 
-cd $PROJECT_ROOT/.dev/unrest/; gulp>/dev/null
-cd $PROJECT_ROOT/.dev/drop/; gulp>/dev/null
-cd $PROJECT_ROOT; gulp>/dev/null
+# if [[ ! -z $MIGRATIONS ]];
+#     then
+#     echo "migrating because of git pull output:"
+#     echo $MIGRATIONS
+#     cd $PROJECT_ROOT
+#     python manage.py migrate >/dev/null
+# fi
+
+# cd $PROJECT_ROOT/.dev/unrest/; gulp>/dev/null
+# cd $PROJECT_ROOT/.dev/drop/; gulp>/dev/null
+# cd $PROJECT_ROOT; gulp>/dev/null
 
 python $PROJECT_ROOT/manage.py collectstatic --noinput>/dev/null
 python $PROJECT_ROOT/manage.py compress>/dev/null
