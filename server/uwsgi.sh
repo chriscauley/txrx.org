4786ea8ba7cd4db42a70a2e43156ffdb1da91d21
chriscauley <chris@lablackey.com>
2019-09-04 02:30:38 +0000

    tweaking uwsgi script a bit

--- server/uwsgi.sh
+++ server/uwsgi.sh
@@ -5,14 +5,15 @@ then
     export PROCESSES=1
 fi
 
-if cat /tmp/txrx.pid 2> /dev/null
+PID=`cat /tmp/txrx.pid 2>/dev/null`
+
+if kill -HUP $PID
 then
-    kill -HUP `cat /tmp/txrx.pid`
     echo "uwsgi reset"
 else
     source .venv/bin/activate
     echo "uwsgi wasn't running, starting it now"
-    uwsgi -M -x $DIR/../uwsgi.xml &&
+    uwsgi -M -x $DIR/../uwsgi.xml &
 fi
 
 exit 0
